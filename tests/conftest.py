17fc7e763366b8f08001a8a0ccb298e63ec0a5d7
Va dumb <vanyabagmetov@gmail.com>
2026-09-17 23:11:30 +0300

    add: добавлены фикстуры вход в аккаунт: admin,manager. фикстура получения категории товара, фикстура создания товара

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
-#tests/conftest.py
 import requests
 import pytest
 
-from api.api_manager import ApiManager
+from api.api_manager import ApiManager, CategoriesAPI
+from data.product import ProductData
 from data.users import UserData
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def api_manager():
     session = requests.Session()
     yield ApiManager(session)
@@ -23,7 +23,19 @@ def registered_user(api_manager):
     response = api_manager.auth_api.register_user(user_data)
     return {**user_data, "id": response.json()["id"]}
 
-@pytest.fixture(scope="function")
+@pytest.fixture
+def registered_manager(api_manager):
+    user_data = UserData.registration_manager_data()
+    response = api_manager.auth_api.register_user(user_data)
+    return {**user_data, "id": response.json()["id"]}
+
+@pytest.fixture
+def registered_admin(api_manager):
+    user_data = UserData.registration_admin_data()
+    response = api_manager.auth_api.register_user(user_data)
+    return {**user_data, "id": response.json()["id"]}
+
+@pytest.fixture
 def authenticated_user(api_manager):
     user_data = UserData.registration_data()
 
@@ -33,3 +45,38 @@ def authenticated_user(api_manager):
 
     return {**user_data, "id": register_response.json()["id"]}
 
+@pytest.fixture
+def authenticated_manager(api_manager):
+    user_data = UserData.registration_manager_data()
+
+    register_response = api_manager.auth_api.register_user(user_data)
+
+    api_manager.auth_api.authenticate((user_data["email"], user_data["password"]))
+
+    return {**user_data, "id": register_response.json()["id"]}
+
+@pytest.fixture
+def authenticated_admin(api_manager):
+    user_data = UserData.registration_admin_data()
+
+    register_response = api_manager.auth_api.register_user(user_data)
+
+    api_manager.auth_api.authenticate((user_data["email"], user_data["password"]))
+
+    return {**user_data, "id": register_response.json()["id"]}
+
+@pytest.fixture
+def category_id(api_manager):
+    response = api_manager.categories_api.get_categories()
+
+    categories = response.json()
+    assert categories, "Список пуст"
+    return categories[-1]["id"]
+
+@pytest.fixture
+def created_product(api_manager, authenticated_admin, category_id):
+    product_data = ProductData.creation_product_data(category_id)
+    response = api_manager.products_api.create_product(product_data)
+    product = {**product_data, "id": response.json()["id"]}
+    yield product
+    api_manager.products_api.delete_product(product["id"])
