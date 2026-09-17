eae8da024373b27684a341cbc74fd035b6fae806
Va dumb <vanyabagmetov@gmail.com>
2026-09-17 20:36:36 +0300

    fix: изменил в методе creation_product_data название функции на верное после изменения в DataGenerator

--- data/product.py
+++ data/product.py
@@ -6,9 +6,9 @@ class ProductData:
     """Object Mother для тел запросов product API"""
 
     @staticmethod
-    def creation_product_data(category_id):
+    def creation_product_data(category_id) -> dict:
         return {
-            "name": DataGenerator.generate_gadget_name(),
+            "name": DataGenerator.generate_product_name(),
             "sku": DataGenerator.generate_sku(),
             "description": DataGenerator.generate_description(),
             "price": DataGenerator.generate_price(),
